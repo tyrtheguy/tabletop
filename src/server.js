7266bf6ea2f91249c48e475dd0bc10ca5fa9821e
Davi <celpekun@gmail.com>
2025-11-15 12:09:13 -0300

    first commit

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 90001;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT} port.`);
+    console.log(`listening ${PORT}`);
 });
